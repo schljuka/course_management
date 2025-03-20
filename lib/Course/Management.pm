72bc75280e0b16b828004642e123e8d9fb66deec
Ljubomir Nedic <nljuka@yahoo.com>
2025-03-20 10:31:27 +0100

    add course configuration file and load it

--- lib/Course/Management.pm
+++ lib/Course/Management.pm
@@ -1,11 +1,19 @@
 package Course::Management;
 use Mojo::Base 'Mojolicious', -signatures;
+use YAML qw(LoadFile);
+use Mojo::Home;
+use Mojo::Home;
 
 # This method will run once at server start
 sub startup ($self) {
 
   # Load configuration from config file
   my $config = $self->plugin('NotYAMLConfig');
+
+  my $home=Mojo::Home->new;
+  $home->detect;
+
+  my $config_course = LoadFile($home->child($config->{course_config}));
 
   # Configure the application
   $self->secrets($config->{secrets});
@@ -14,7 +22,7 @@ sub startup ($self) {
   my $r = $self->routes;
 
   # Normal route to controller
-  $r->get('/')->to('Example#welcome');
+  $r->get('/' => {cc=>$config_course})->to('Example#welcome');
 }
 
 1;
